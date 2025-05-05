8b5fe1a42586027db5c5f9a58b85335ed5c1163f
Vrushti Patel <vrushtipatel1706@gmail.com>
2025-05-04 21:40:57 -0400

    challenge 6

--- index.js
+++ index.js
@@ -154,12 +154,18 @@ allBtn.addEventListener("click", showAllGames);
 const descriptionContainer = document.getElementById("description-container");
 
 // use filter or reduce to count the number of unfunded games
-
+let listOfUnfundedGames = GAMES_JSON.filter( (game) => {
+    return game.pledged < game.goal;
+});
+const numUnfundedGames = listOfUnfundedGames.length;
 
 // create a string that explains the number of unfunded games using the ternary operator
-
+const summaryString = `A total of $${totalRaised.toLocaleString()} has been raised for ${GAMES_JSON.length} games. Currently, ${numUnfundedGames} game${numUnfundedGames == 1 ? '' : 's'} remain unfunded. We need your help to fund these amazing games!`;
 
 // create a new DOM element containing the template string and append it to the description container
+const summaryParagraph = document.createElement("p");
+summaryParagraph.textContent = summaryString;
+descriptionContainer.appendChild(summaryParagraph);
 
 /************************************************************************************
  * Challenge 7: Select & display the top 2 games
